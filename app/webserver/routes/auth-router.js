ba8ade390fd6e6862a75d3b7b2a93357b4c74874
braiscaloto <braiscaloto92@gmail.com>
2020-03-04 23:52:36 +0100

    m

--- app/webserver/routes/auth-router.js
+++ app/webserver/routes/auth-router.js
@@ -6,8 +6,5 @@ const login = require('../controllers/auth/auth-login-controller');
 const router = express.Router();
 
 router.post('/auth', login);
-router.get('/auth', (req, res) => {
-  res.send('Hello Heroku');
-});
 
 module.exports = router;
